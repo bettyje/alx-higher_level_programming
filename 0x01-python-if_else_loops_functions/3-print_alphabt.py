bb74410eba09b17af236f9389545538c960333d0
bettyje <bettyjelagat156@gmail.com>
2023-06-19 06:43:32 +0000

    prints all numbers from 0 to 98 in decimal and in hexadecimal

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for ch in range(98, 124):
-        if ch != 102 and ch != 114:
+for ch in range(97, 123):
+        if ch != 101 and ch != 113:
                 print("{:c}".format(ch), end='')
