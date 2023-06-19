f96b9c47158c07c4ee869e2bbe7e0b2be2af89ab
bettyje <bettyjelagat156@gmail.com>
2023-06-19 08:20:09 +0000

    Adding more files.it works!

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-for num1 in range(10):
-    for num2 in range(num1+1, 10):
-        print("{:d}{:d}".format(num1, num2), end=", ")
-print()
+for num in range(0, 90):
+        if num % 10 > num / 10:
+                if num != 89:
+                        print("{:02d}, ".format(num), end='')
+                else:
+                        print("{:02d}".format(num))
 
